b232a4ab2c8b448c200a5ad5cb976bcc178aa681
Himanshu Raj <himanshuraj6771@gmail.com>
2024-02-24 19:36:06 +0530

    Update Firebase.jsx

--- react-frontend/src/Firebase.jsx
+++ react-frontend/src/Firebase.jsx
@@ -1,15 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 
-
+//firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAK99Wy55H3V9CXvP5KPoX0YEeNVrCNOqI",
-  authDomain: "coastal-set-410613.firebaseapp.com",
-  projectId: "coastal-set-410613",
-  storageBucket: "coastal-set-410613.appspot.com",
-  messagingSenderId: "1056719764103",
-  appId: "1:1056719764103:web:7398d5314def147c8cd622",
-  measurementId: "G-PM9Z8SDXFX"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
